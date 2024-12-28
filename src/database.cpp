c959dcbfa1e68ebed2a9a6248a9bb9884e5dd5aa
m0rgi0n999 <m0rgi0n999@gmail.com>
2024-12-28 16:32:11 +0100

    1

--- src/database.cpp
+++ src/database.cpp
@@ -1,11 +1,12 @@
 #include "database.h"
 #include "monster.h"
+#include "utils.h" // Include the utils header
 #include <iostream>
 #include <fstream>
 #include <vector>
 #include <string>
 #include <algorithm>
-#include <sstream> // Include the sstream header
+#include <sstream>
 
 Database::Database() {}
 
@@ -30,10 +31,12 @@ void Database::importMonsters(const std::string& filename) {
     std::string line;
     while (std::getline(file, line)) {
         // Assuming the line format is "name,type,hitDice,armorClass"
-        std::istringstream iss(line);
-        std::string name, type;
-        int hitDice, armorClass;
-        if (iss >> name >> type >> hitDice >> armorClass) {
+        std::vector<std::string> tokens = split(line, ',');
+        if (tokens.size() == 4) {
+            std::string name = tokens[0];
+            std::string type = tokens[1];
+            int hitDice = std::stoi(tokens[2]);
+            int armorClass = std::stoi(tokens[3]);
             addMonster(Monster(name, type, hitDice, armorClass));
         }
     }
