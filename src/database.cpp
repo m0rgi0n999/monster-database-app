cc4546b1e84d89b4a4e1d3d6db978aa1481c7452
m0rgi0n999 <m0rgi0n999@gmail.com>
2024-12-28 16:27:04 +0100

    error in database.cpp

--- src/database.cpp
+++ src/database.cpp
@@ -6,47 +6,41 @@
 #include <string>
 #include <algorithm>
 
-class Database {
-public:
-    Database() {}
+Database::Database() {}
 
-    ~Database() {}
+Database::~Database() {}
 
-    void addMonster(const Monster& monster) {
-        monsters.push_back(monster);
+void Database::addMonster(const Monster& monster) {
+    monsters.push_back(monster);
+}
+
+void Database::removeMonster(const std::string& name) {
+    monsters.erase(std::remove_if(monsters.begin(), monsters.end(),
+        [&name](const Monster& m) { return m.getName() == name; }), monsters.end());
+}
+
+void Database::importMonsters(const std::string& filename) {
+    std::ifstream file(filename);
+    if (!file.is_open()) {
+        std::cerr << "Could not open the file: " << filename << std::endl;
+        return;
     }
 
-    void removeMonster(const std::string& name) {
-        monsters.erase(std::remove_if(monsters.begin(), monsters.end(),
-            [&name](const Monster& m) { return m.getName() == name; }), monsters.end());
-    }
-
-    void importMonsters(const std::string& filename) {
-        std::ifstream file(filename);
-        if (!file.is_open()) {
-            std::cerr << "Could not open the file: " << filename << std::endl;
-            return;
-        }
-
-        std::string line;
-        while (std::getline(file, line)) {
-            // Assuming the line format is "name,type,hitDice,armorClass"
-            std::istringstream iss(line);
-            std::string name, type;
-            int hitDice, armorClass;
-            if (iss >> name >> type >> hitDice >> armorClass) {
-                addMonster(Monster(name, type, hitDice, armorClass));
-            }
+    std::string line;
+    while (std::getline(file, line)) {
+        // Assuming the line format is "name,type,hitDice,armorClass"
+        std::istringstream iss(line);
+        std::string name, type;
+        int hitDice, armorClass;
+        if (iss >> name >> type >> hitDice >> armorClass) {
+            addMonster(Monster(name, type, hitDice, armorClass));
         }
     }
+}
 
-    void displayMonsters() const {
-        for (const auto& monster : monsters) {
-            std::cout << "Name: " << monster.name << ", Type: " << monster.type
-                      << ", Hit Dice: " << monster.hitDice << ", Armor Class: " << monster.armorClass << std::endl;
-        }
+void Database::displayMonsters() const {
+    for (const auto& monster : monsters) {
+        std::cout << "Name: " << monster.name << ", Type: " << monster.type
+                  << ", Hit Dice: " << monster.hitDice << ", Armor Class: " << monster.armorClass << std::endl;
     }
-
-private:
-    std::vector<Monster> monsters;
-};
+}
