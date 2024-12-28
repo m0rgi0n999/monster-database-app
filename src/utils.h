3e80b04b79e29b4e4d6c1356893254b36e50b284
m0rgi0n999 <m0rgi0n999@gmail.com>
2024-12-28 16:31:08 +0100

    missing utils

--- src/utils.h
+++ src/utils.h
@@ -7,5 +7,6 @@
 
 std::vector<std::string> readFile(const std::string& filename);
 bool validateInput(const std::string& input);
+std::vector<std::string> split(const std::string& s, char delimiter);
 
 #endif // UTILS_H
